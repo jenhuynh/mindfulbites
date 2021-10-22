1f46cc2c138eac7a7d02135590359449eacefa8e
jenhuynh <jendoubleh@gmail.com>
2021-10-22 12:51:56 -0700

    Add new component called moodtracker

---
+++ app/src/MoodTracker/index.js
@@ -0,0 +1,10 @@
+// import * as React from "react";
+
+// import useApi from "../auth/useApi";
+
+// const MoodTracker = () => {
+//     const [moodTracker, setMoodTracker] = React.useState([]);
+//     const {
+//         loadMoodTracker(await apiClient.addMood)
+//     }
+// };
