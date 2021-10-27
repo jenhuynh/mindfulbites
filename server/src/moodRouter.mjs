53edad62851bf5c0f04aea886d2fbcc40f1b83c8
jenhuynh <jendoubleh@gmail.com>
2021-10-27 16:26:44 -0700

    add additional routes for resource and getresult and getresourcebyid

--- server/src/moodRouter.mjs
+++ server/src/moodRouter.mjs
@@ -10,17 +10,39 @@ moodRouter.get("/", async (request, response) => {
 });
 
 moodRouter.get("/latest", async (request, response) => {
+  //getting mood from the database
   const latestMood = await db.getLatestMood(request.user.sub);
+  console.log("=====");
+  console.log(latestMood);
+  //get result from the database that displays a quote
+  const result = await db.getResult({ mood_id: latestMood.id });
+  console.log(result);
+  //get resource from the database that display a link
+  const resource = await db.getResourceById({ id: result.resource_id });
+  console.log(resource);
+  console.log("*****");
   response.json(latestMood);
   console.log(request.user.sub);
 });
 
+//creation time
 moodRouter.use(express.json());
 moodRouter.post("/", async (request, response) => {
   //saves a mood to the database
   const mood = await db.addMood(request.user.sub, request.body);
-  //find an appropriate resource object that has a correct mood (make one at random) -- write a new method in db.mjs
-  //use resource object to create a new result
+  //when the user produces a mood, it should auto populate a video link and quote based on that mood
+  const resource = await db.getResource({ mood: mood.current_mood });
+  console.log(resource);
+  await db.addResult({
+    mood_id: mood.id,
+    //use resource object to create a new result
+    resource_id: resource.id,
+    quote: "Hello",
+  });
+  //end result we need to insert a new result into the result table
+
+  //find an appropriate resource object that has a correct mood (pick one at random) -- write a new method in db.mjs
+
   console.log(request.body);
   console.log(request.user.sub);
   response.status(201).json(mood);
