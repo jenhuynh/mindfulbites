6fdd863f286e9835c52c94c6bef4fc66d22cc141
jenhuynh <jendoubleh@gmail.com>
2021-10-27 08:13:39 -0700

    add comment about single mood query

--- server/src/db.mjs
+++ server/src/db.mjs
@@ -18,13 +18,22 @@ export const getMoods = (sub) =>
     { sub },
   );
 
-//displays single mood they selected for the day
+//displays single mood they selected for the day - working
 export const getLatestMood = (sub) =>
   db.any(
     "SELECT moodrecord.* FROM moodrecord LEFT JOIN users on user_id=users.id WHERE sub=$<sub> ORDER BY timestamp DESC LIMIT 1;",
     { sub },
   );
 
+//display the resource based on users id and mood??
+//this query returns resource id 2 with mood wonderful
+export const getResource = (sub, { link, mood }) =>
+  db.any(
+    "SELECT resource.* FROM resource LEFT JOIN users on user_id=users.id WHERE sub=$<sub> ORDER BY timestamp DESC LIMIT 1;",
+    { sub, link, mood },
+  );
+
+//display all resources
 export const addTask = (sub, name) =>
   db.one(
     `INSERT INTO tasks(user_id, name)
@@ -33,6 +42,7 @@ export const addTask = (sub, name) =>
     { sub, name },
   );
 
+//addmood from form to db
 export const addMood = (sub, { current_mood, notes, photo, timestamp }) =>
   db.one(
     `INSERT INTO moodrecord( user_id, current_mood, notes, photo, timestamp)
@@ -40,6 +50,10 @@ export const addMood = (sub, { current_mood, notes, photo, timestamp }) =>
       RETURNING *`,
     { sub, current_mood, notes, photo, timestamp },
   );
+
+//shows results from result table based on mood
+// export const addResult = (sub, {mood_id, resource_id, quote }) =>
+// db.one(`INSERT INTO resource ()`{})
 
 export const addOrUpdateUser = (user) =>
   db.one(
