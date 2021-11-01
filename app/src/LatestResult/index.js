5af24b3b2b07394ffb662c8661bfdc6e07aff998
jenhuynh <jendoubleh@gmail.com>
2021-11-01 16:37:17 -0700

    add innerhtml element in quote of the day

--- app/src/LatestResult/index.js
+++ app/src/LatestResult/index.js
@@ -57,7 +57,12 @@ const LatestResultDetail = ({ latestResult }) => (
         ></iframe>
       </div>
       <div>
-        <p>Quote of the day: {latestResult.result.quote}</p>
+        <p dangerouslySetInnerHTML={{ __html: latestResult.result.quote }} />
+        {/* <p>Quote of the day: {latestResult.result.quote}</p> */}
+        Inspirational quotes provided by{" "}
+        <a href="https://zenquotes.io/" target="_blank" rel="noreferrer">
+          ZenQuotes API
+        </a>
       </div>
     </div>
   </>
