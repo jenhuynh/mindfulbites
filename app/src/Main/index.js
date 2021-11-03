12020e001dc138694814b905ecd85e6730171d0b
jenhuynh <jendoubleh@gmail.com>
2021-11-02 19:13:52 -0700

    import Main component and import login

---
+++ app/src/Main/index.js
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+import { Login } from "../auth/widgets";
+
+const Main = () => {
+  return (
+    <>
+      <h1>Mindful Bites</h1>
+      <Login />
+    </>
+  );
+};
+
+export default Main;
