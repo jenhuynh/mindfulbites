63e59e9b9306d7795183e98b7bd99c23bb296e26
jenhuynh <jendoubleh@gmail.com>
2021-11-04 15:50:14 -0700

    add classnames and css to main component

--- app/src/Main/index.js
+++ app/src/Main/index.js
@@ -2,11 +2,16 @@ import * as React from "react";
 
 import { Login } from "../auth/widgets";
 
+import styles from "./styles.module.scss";
 const Main = () => {
   return (
     <>
-      <h1>Mindful Bites</h1>
-      <Login />
+      <div className={styles.bodymain}>
+        <h1 className={styles.mainh1}>Mindful Bites</h1>
+        <div className={styles.mainbtn}>
+          <Login />
+        </div>
+      </div>
     </>
   );
 };
