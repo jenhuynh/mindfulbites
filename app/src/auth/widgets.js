6c8a8804d17718bb07a19406dd371e33374c55a0
jenhuynh <jendoubleh@gmail.com>
2021-11-04 15:49:26 -0700

    import styles and classnames

--- app/src/auth/widgets.js
+++ app/src/auth/widgets.js
@@ -1,18 +1,29 @@
 import { withAuthenticationRequired } from "@auth0/auth0-react";
 
+import styles from "./styles.module.scss";
 import useAuth0 from "./useAuth0";
-
 export const Login = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <button onClick={loginWithRedirect}>Log in</button>;
+  return (
+    <>
+      <div>
+        <button className={styles.widgetsbtn} onClick={loginWithRedirect}>
+          LOG IN
+        </button>
+      </div>
+    </>
+  );
 };
 
 export const Logout = () => {
   const { logout } = useAuth0();
 
   return (
-    <button onClick={() => logout({ returnTo: window.location.origin })}>
+    <button
+      onClick={() => logout({ returnTo: window.location.origin })}
+      className={styles.authLogout}
+    >
       Log out
     </button>
   );
