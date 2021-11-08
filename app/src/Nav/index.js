47e07b1bda03e5e978ce5dfba41b6b1d3832f195
jenhuynh <jendoubleh@gmail.com>
2021-11-07 17:12:57 -0800

    change background image

--- app/src/Nav/index.js
+++ app/src/Nav/index.js
@@ -7,14 +7,14 @@ import styles from "./styles.module.scss";
 const Nav = () => (
   <nav className={styles.nav}>
     <NavLink to="/about" className={styles.historynav}>
-      About
+      About |
     </NavLink>
     <NavLink to="/" className={styles.historynav}>
-      Add Mood
+      Add Mood |
     </NavLink>
 
     <NavLink to="/history" className={styles.historynav}>
-      Mood History
+      Mood History |
     </NavLink>
     <Logout />
   </nav>
