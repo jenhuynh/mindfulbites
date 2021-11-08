c054385de8d70a2084f4e872802987923489af30
George Song <george@damacy.net>
2021-11-07 19:23:40 -0800

    build: update Heroku related scripts

--- bin/heroku-deploy.mjs
+++ bin/heroku-deploy.mjs
@@ -1,7 +1,7 @@
 import fs from "fs";
 import path from "path";
 import { URL } from "url";
-import { execSync } from "child_process";
+import { spawnSync } from "child_process";
 
 import dotenv from "dotenv";
 
@@ -10,7 +10,7 @@ const DOTENV_FILE = path.join(
   "../app/.env",
 );
 
-let args;
+let args = {};
 
 if (fs.existsSync(DOTENV_FILE)) {
   args = dotenv.parse(fs.readFileSync(DOTENV_FILE));
@@ -20,23 +20,16 @@ const argString = Object.entries(args)
   .map(([key, value]) => `${key}="${value}"`)
   .join(",");
 
-exec("heroku container:login");
-exec(
-  `DOCKER_DEFAULT_PLATFORM=linux/amd64 heroku container:push web --arg ${argString}`,
-);
-exec("heroku container:release web");
+let containerPush =
+  "DOCKER_DEFAULT_PLATFORM=linux/amd64 heroku container:push web";
 
-function exec(cmd) {
+if (argString !== "") containerPush += `  --arg ${argString}`;
+
+spawn("heroku container:login");
+spawn(containerPush);
+spawn("heroku container:release web");
+
+function spawn(cmd) {
   console.info(cmd);
-  execSync(cmd, (error, stdout, stderr) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-    console.log(`stdout: ${stdout}`);
-  });
+  spawnSync(cmd, { shell: true, stdio: "inherit" });
 }
