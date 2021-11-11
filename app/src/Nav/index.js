d75df5ae927b05be439c3f711a596f03781a3178
jenhuynh <jendoubleh@gmail.com>
2021-11-10 17:33:45 -0800

    add new classname for about link

--- app/src/Nav/index.js
+++ app/src/Nav/index.js
@@ -7,7 +7,10 @@ import styles from "./styles.module.scss";
 const Nav = () => (
   <div className={styles.nav}>
     <h3 className={styles.navh3}>Mindful Bites</h3>
-    <NavLink to="/about" className={styles.historynav}>
+    <NavLink
+      to="/about"
+      className={(styles.historynav, styles.historynavabout)}
+    >
       About
     </NavLink>
     <NavLink to="/" className={styles.historynav}>
